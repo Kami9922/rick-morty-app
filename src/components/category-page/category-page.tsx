499d439a9232943448f33a0220735e93e1bb1a33
kami <kami-vi@mail.ru>
2025-05-30 14:48:40 +0400

    feat(catergory-page): implement category-page

--- src/components/category-page/category-page.tsx
+++ src/components/category-page/category-page.tsx
@@ -1,5 +1,33 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import charactersData from '../../db/characters.json'
+import episodesData from '../../db/episodes.json'
+import locationsData from '../../db/locations.json'
 
-export const CategoryPage = () => {
-	return <div>NavPanel</div>
+export const CategoryPage = ({ category }: { category: string }) => {
+	return (
+		<>
+			<h1>{category.charAt(0).toUpperCase() + category.slice(1)}</h1>
+			<ul>
+				{category === 'characters' &&
+					charactersData.map((item) => (
+						<Link to={`/${category}/${item.id}`}>
+							<li>{item.name}</li>
+						</Link>
+					))}
+
+				{category === 'episodes' &&
+					episodesData.map((item) => (
+						<Link to={`/${category}/${item.id}`}>
+							<li>{item.name}</li>
+						</Link>
+					))}
+				{category === 'locations' &&
+					locationsData.map((item) => (
+						<Link to={`/${category}/${item.id}`}>
+							<li>{item.name}</li>
+						</Link>
+					))}
+			</ul>
+		</>
+	)
 }
