19653cb6facf89c25de98950c6e45a604a3d2c55
kami <kami-vi@mail.ru>
2025-05-30 14:47:47 +0400

    feat(nav-layout): implement nav-layout

--- src/layout/nav-layout.tsx
+++ src/layout/nav-layout.tsx
@@ -3,15 +3,17 @@ import { Link, Outlet } from 'react-router-dom'
 export const NavLayout = () => {
 	return (
 		<>
+			<h1>Hello to all fans of Rick and Morty!</h1>
+			<h3>Nav Panel</h3>
 			<ul>
 				<li>
-					<Link to='#'>Персонажи</Link>
+					<Link to='/characters'>Characters</Link>
 				</li>
 				<li>
-					<Link to='#'>Эпизоды</Link>
+					<Link to='/episodes'>Episodes</Link>
 				</li>
 				<li>
-					<Link to='#'>Локации</Link>
+					<Link to='/locations'>Locations</Link>
 				</li>
 			</ul>
 			<Outlet />
